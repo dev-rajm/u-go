4520f448896869364a943630a5befdd5b2d1a836
rajmanna <dev.rajm8923@gmail.com>
2025-07-24 18:47:40 +0530

    Replace anchor tag with React Link in Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,12 +4,12 @@ import ThemeToggle from './ThemeToggle';
 const Navbar = () => {
   return (
     <nav className="flex justify-between h-[3.7rem] px-3 lg:px-8 bg-white">
-      <div className="flex items-center">
+      <Link to="/" className="flex items-center">
         <img src="/logo.png" alt="Black colored tick mark" className="size-8" />
         <h1 className="text-3xl ml-1 font-medium -tracking-wider">
           U <span className="text-green-500">Go</span>
         </h1>
-      </div>
+      </Link>
       <div className="flex items-center space-x-3 lg:space-x-8">
         <ThemeToggle />
         <Link
